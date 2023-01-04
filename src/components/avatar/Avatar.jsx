32f26b0a92ecc88f9f7f1c4b3db9bdb7cef89a77
Valentin Vladimirov <valentinvladimirov@abv.bg>
2023-01-05 01:53:59 +0200

    added Card component

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,21 +1,9 @@
 import * as React from "react";
 
-// mui components
-// import Badge from "@mui/material/Badge";
-// import Avatar from "@mui/material/Avatar";
-// import Stack from "@mui/material/Stack";
-
 // styles
-//import { styled } from "@mui/material/styles";
 import classNames from "classnames";
 import styles from "./Avatar.module.scss";
 
-// const Verified = styled(Avatar)(({ theme }) => ({
-//     width: theme.width,
-//     height: theme.height,
-// }));
-
-//export default function AvatarImage({ url, size = 90, verified = false }) {
 export default function Avatar({ url, size = 90, verified = false }) {
     return (
         <div className={classNames(styles.avatar)} width={size} height={size}>
@@ -34,24 +22,5 @@ export default function Avatar({ url, size = 90, verified = false }) {
                 height={size / 4}
             />
         </div>
-        // <Stack direction="row" spacing={2}>
-        //     <Badge
-        //         overlap="circular"
-        //         anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        //         badgeContent={
-        //             <Verified
-        //                 alt="verified icon"
-        //                 src="../images/verified.svg"
-        //                 sx={{ width: size / 4, height: size / 4 }}
-        //             />
-        //         }
-        //     >
-        //         <Avatar
-        //             alt="Avatar"
-        //             src={url}
-        //             sx={{ width: size, height: size }}
-        //         />
-        //     </Badge>
-        // </Stack>
     );
 }
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import millify from "millify";
+
+// mui components
+import { default as MuiCard } from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Favorite from "@mui/icons-material/Favorite";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+
+// components
+import Avatar from "../avatar/Avatar";
+
+// styles
+import { styled } from "@mui/material/styles";
+import styles from "./Card.module.scss";
+import classNames from "classnames";
+
+// grid item
+const Item = styled(Container)(({ theme }) => ({
+    //backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.primary,
+}));
+
+export default function Card({
+    name = "",
+    likes = 0,
+    mediaUrl = "",
+    user = { avatar: { url: "" }, verified: false },
+    price = "",
+    currency = "",
+}) {
+    return (
+        <MuiCard
+            className={classNames(styles.card)}
+            sx={{ maxWidth: 325, borderRadius: 0.5 }}
+        >
+            <CardHeader
+                sx={{ paddingLeft: 0 }}
+                avatar={
+                    <Avatar
+                        url={user.avatar.url}
+                        size={33}
+                        verified={user.verified}
+                    />
+                }
+                title=""
+                subheader=""
+            />
+            <CardMedia
+                className={classNames(styles.media)}
+                component="img"
+                height="286"
+                image={mediaUrl}
+                alt="nft"
+            />
+            <CardContent>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={3}>
+                        <Grid item>
+                            <Item>
+                                <h5 className={classNames(styles.title)}>
+                                    {name}
+                                </h5>
+                                <p className={classNames(styles.price)}>
+                                    {price} {currency}
+                                </p>
+                            </Item>
+                        </Grid>
+                        <Grid item>
+                            <div className={classNames(styles.likes)}>
+                                <Button
+                                    sx={{
+                                        padding: 0.6,
+                                        width: 1,
+                                        color: "#24f25e",
+                                        border: "1px solid #24f25e",
+                                        backgroundColor:
+                                            "rgba(36, 242, 94, 0.1)",
+                                        fontFamily: "Montserrat",
+                                        fontStyle: "normal",
+                                        fontWeight: 700,
+                                        fontSize: "1rem",
+                                        lineHeight: "1rem",
+                                    }}
+                                    variant="outlined"
+                                    startIcon={<Favorite />}
+                                >
+                                    {millify(likes)}
+                                </Button>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </CardContent>
+        </MuiCard>
+    );
+}
